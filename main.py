63f9f73481361763516b832a5f952743d4559cd2
ariaaay <ariayuanwang@gmail.com>
2018-11-08 17:42:48 -0500

    add functions to avg brain data if repeated

--- main.py
+++ main.py
@@ -8,7 +8,25 @@ def test_2v2_accuracy():
     pass
 
 def avg_repeated_brain_trails(brain_data, brain_labels):
-    pass
+    """
+    Average the duplicated trials in the brain data
+    """
+    brain_labels_unique = []
+    label2trial = dict()
+    for i, l in enumerate(brain_labels):
+        if l not in brain_labels_unique:
+            brain_labels_unique.append(l)
+            label2trial[l] = i
+        else:
+            label2trial[l].append(i)
+    
+    brain_data_unique = np.zeros(len(brain_labels_unique), brain_data.shape[1])
+    for j, lab in enumerate(brain_labels):
+        if len(label2trial[lab]) == 1:
+            brain_data_unique[j,:] = brain_data[label2trial[lab],:]
+        else:
+            brain_data_unique[j,:] = np.mean([brain_data[idx] for idx in label2trial[lab]])
+    return brain_data_unique, brain_labels_unique
 
 def extract_common_objs(brain_data, brain_labels, obj_vectors, obj_labels):
     """
@@ -84,6 +102,7 @@ if __name__ == '__main__':
     wv_model = KeyedVectors.load(obj_embedding_path, mmap='r')
     obj_vectors = wv_model.vectors
     obj_labels = list(wv_model.vocab)
-    X, Y = extract_common_objs(brain_data, brain_labels, obj_vectors, obj_labels)
+    brain_data_unique, brain_labels_unique = avg_repeated_brain_trails(brain_data, brain_labels)
+    X, Y = extract_common_objs(brain_data_unique, brain_labels_unique, obj_vectors, obj_labels)
 
     main(brain_data, brain_labels, pix2vec, wv_list)
