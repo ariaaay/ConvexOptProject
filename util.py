6c766e5a7dd7763ca43a2ec1fe3149b3ab02251f
ariaaay <ariayuanwang@gmail.com>
2018-12-08 20:38:46 -0500

    add util function

---
+++ util.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+def takeout_repeated_brain_trials(brain_data, brain_labels):
+    """
+    Take out the duplicated trials in the brain data
+    """
+    brain_labels_unique = []
+    label2trial = dict()
+    for i, l in enumerate(brain_labels):
+        if l not in brain_labels_unique:
+            brain_labels_unique.append(l)
+            label2trial[l] = i
+    # print(label2trial)
+
+    brain_data_unique = np.zeros((len(brain_labels_unique), brain_data.shape[1]))
+    for j, lab in enumerate(brain_labels_unique):
+        brain_data_unique[j, :] = brain_data[label2trial[lab], :]
+    return brain_data_unique, brain_labels_unique
+
+def simulate_data(w0, w1, w2, l=100, c=200):
+    A = np.random.rand(w0+w1+w2, l)
+    D_X = np.random.rand(l, c)
+    D_Y = np.random.rand(l, c)
+    X = A[:(w0+w1), :] @ D_X
+    Y = np.vstack((A[:w0, :], A[w0+w1:,:])) @ D_Y
+    return X, Y
+
+
+def extract_common_objs(brain_data, brain_labels, obj_vectors, obj_labels):
+    """
+    This functions takes in NON-repeated brain data.
+    Outputs:
+    - X: object embedding matrix of dimension w1-by-c. First w rows of X corresponds to objects
+    that are also present in the brain data, in the same order as it is present in the brain data.
+    - Xlabels: labels corresponding to the rows in X.
+    - Y: the brain data matrix of dimension w2-by-v.
+    - Ylabels: labels corresponding to the rows in Y.
+    - w: number of words that overlap
+    """
+    br_overlap_idx = []
+    obj_overlap_idx = []
+
+    for i, obj_lab in enumerate(obj_labels):
+        for j, br_lab in enumerate(brain_labels):
+            if obj_lab == br_lab:
+                obj_overlap_idx.append(i)
+                br_overlap_idx.append(j)
+    assert(len(obj_overlap_idx) == len(br_overlap_idx))
+    w = len(obj_overlap_idx)
+
+    br_nonoverlap_idx = np.setdiff1d(np.arange(len(brain_labels)), br_overlap_idx)
+    obj_nonoverlap_idx = np.setdiff1d(np.arange(len(obj_labels)), obj_overlap_idx)
+
+    X = np.vstack((obj_vectors[obj_overlap_idx,:], obj_vectors[obj_nonoverlap_idx,:]))
+    Y = np.vstack((brain_data[br_overlap_idx,:], brain_data[br_nonoverlap_idx,:]))
+
+    return X, Y, w
