624f8453abaffe233be7aaaeb2cfbaa69d11d9e3
ariaaay <ariayuanwang@gmail.com>
2018-12-11 14:47:02 -0500

    for cluster

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+from tqdm import tqdm
 import pickle
 import spams
 import sys
@@ -72,35 +73,99 @@ def plot_rdm(X, Y, w):
 #     # Train a model XW = A using L2-regularized linear regression.
 #     # Use trained W to compute 2v2 accuracy on a holdout set.
 
-def main(X, Y, w, fit_algo, transform_algo, K=100, lamb=0.025):
+def joint_optimize(X, Y, w, w1, fit_algo, transform_algo, K=100, lamb=0.1):
     model_X, D_Y = None, None
     params = {'n_components':K, 'alpha':lamb, 'max_iter': 100, 'n_jobs': -1, 'positive_code':True,
-              'transform_algorithm':transform_algo, 'fit_algorithm':fit_algo, 'tol':1e-05}
+              'transform_algorithm':transform_algo, 'fit_algorithm':fit_algo, 'tol':1e-02}
     t = 0
     loss_X_arr = []
     loss_Y_arr = []
     converged = False
     tol = 1e-3
     curr_obj = np.inf
-    while not converged and t < 100:
+    while not converged and t < 300:
+        prev_obj = curr_obj
+
+        if model_X is None:
+            model_X = DictionaryLearning(**params)
+            model_joint = DictionaryLearning(**params)
+            model_Y = DictionaryLearning(**params)
+        else:
+            model_X = DictionaryLearning(code_init=A_X, dict_init=D_X, **params)
+            model_Y = DictionaryLearning(code_init=A_Y, dict_init=D_Y, **params)
+            model_joint = DictionaryLearning(code_init=A, dict_init = np.hstack((D_X, D_Y)), **params)
+        
+        model_X.fit(X)
+        A_X = model_X.transform(X)
+        D_X = model_X.components_
+
+        model_Y.fit(Y)
+        A_Y = model_Y.transform(Y)
+        D_Y = model_Y.components_
+
+        model_joint.fit(np.hstack((X[:w,:], Y[:w,:])))
+        A_joint = model_joint.transform(np.hstack((X[:w,:], Y[:w,:])))
+
+        A = np.zeros((w+w1+w1, K)) #only in simulation data
+
+        A[:w,:] = A_joint
+        A[w:w+w1,:] = A_X[w:,:]
+        A[w+w1:,:] = A_Y[w:,:]
+        A_X[:w,:] = A[:w,:]
+        A_Y[:w,:] = A[:w,:]
+
+        loss_X = eval(X, A[:w+w1,:], D_X)
+        loss_X_arr.append(loss_X)
+        loss_Y = eval(Y, np.vstack((A[:w,:], A[w+w1:,:])), D_Y)
+        loss_Y_arr.append(loss_Y)
+
+        np.save("./outputs/joint_loss_X_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_X_arr)
+        np.save("./outputs/joint_loss_Y_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_Y_arr)
+
+        t+=1
+        curr_obj = loss_X + loss_Y
+        converged = np.abs(prev_obj - curr_obj) <= tol
+
+
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(t), loss_Y_arr)
+    ax2 = ax.twinx()
+    ax2.plot(np.arange(t), loss_X_arr)
+    plt.savefig("./figures/joint_{}_{}_{}.png".format(transform_algo, fit_algo, lamb))
+
+    return D_X, D_Y, A
+
+def main_optimize(X, Y, w, fit_algo, transform_algo, K=100, lamb=0.025):
+    model_X, D_Y = None, None
+    params = {'n_components': K, 'alpha': lamb, 'max_iter': 500, 'n_jobs': -1, 'positive_code': True,
+              'transform_algorithm': transform_algo, 'fit_algorithm': fit_algo, 'tol': 1e-02}
+    t = 0
+    loss_X_arr = []
+    loss_Y_arr = []
+    converged = False
+    tol = 1e-3
+    curr_obj = np.inf
+    while not converged and t < 300:
         prev_obj = curr_obj
 
         if model_X is None:
             model_X = DictionaryLearning(**params)
         else:
-            A_X[:w,:] = A_Y[:w,:]
+            A_X[:w, :] = A_Y[:w, :]
             model_X = DictionaryLearning(code_init=A_X, dict_init=D_X, **params)
-        
+
         model_X.fit(X)
         A_X = model_X.transform(X)
         D_X = model_X.components_
         loss_X = eval(X, A_X, D_X)
         loss_X_arr.append(loss_X)
-        print('Loss of X: %f' % loss_X)
+        # print('Loss of X: %f' % loss_X)
 
-        #warm start alpha in model_Y
+        # warm start alpha in model_Y
         A_Y = np.zeros((Y.shape[0], K))
-        A_Y[:w,:] = A_X[:w,:]
+        A_Y[:w, :] = A_X[:w, :]
         if D_Y is None:
             model_Y = DictionaryLearning(code_init=A_Y, **params)
         else:
@@ -112,18 +177,17 @@ def main(X, Y, w, fit_algo, transform_algo, K=100, lamb=0.025):
 
         loss_Y = eval(Y, A_Y, D_Y)
         loss_Y_arr.append(loss_Y)
-        print('Loss of Y: %f' % loss_Y)
+        # print('Loss of Y: %f' % loss_Y)
 
-        t+=1
+        t += 1
         curr_obj = loss_X + loss_Y
-        converged = prev_obj - curr_obj <= tol
+        converged = np.abs(prev_obj - curr_obj) <= tol
+        np.save("./outputs/loss_X_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_X_arr)
+        np.save("./outputs/loss_Y_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_Y_arr)
 
     # print(loss_X_arr)
     # print(loss_Y_arr)
-    loss_sum_arr = loss_X_arr+loss_Y_arr
-    np.save("./outputs/loss_X_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_X_arr)
-    np.save("./outputs/loss_Y_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_Y_arr)
-    np.save("./outputs/loss_sum_{}_{}_{}.npy".format(transform_algo, fit_algo, lamb), loss_sum_arr)
+
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -133,6 +197,7 @@ def main(X, Y, w, fit_algo, transform_algo, K=100, lamb=0.025):
     plt.savefig("./figures/{}_{}.png".format(transform_algo, fit_algo))
 
     return D_X, D_Y, A_Y, A_X
+
 
 def eval(X, A, D, lamb=0):
     recon_X = A @ D
@@ -166,6 +231,9 @@ if __name__ == '__main__':
     # plot_rdm(X, Y, w)
     w0, w1, w2 = 200, 100, 100
     Xsim, Ysim, Asim, Dsimx, Dsimy = simulate_data(w0, w1, w2, return_D=True)
+    np.save("Xsim.npy", Xsim)
+    np.save("Ysim.npy", Ysim)
+    np.save("Asim.npy", Asim)
     # Ax = Asim[:w0+w1,:]
     # Ay = np.vstack((Asim[:w0, :], Asim[w0+w1:,:]))
     # sim_loss = eval(Xsim, Ax, Dsimx) + eval(Ysim, Ay, Dsimy)
@@ -174,15 +242,17 @@ if __name__ == '__main__':
 
     transform_algorithm = ['lasso_lars', 'lasso_cd']
     fit_algorithm = ['lars', 'cd']
-    lamb = np.logspace(-2, 1, 4)
+    lambs = np.logspace(-2, 1, 4)
     #
     # transform_algorithm = ['lasso_lars']
     # fit_algorithm = ['lars']
 
-    # for tr in transform_algorithm:
-    #     for ft in fit_algorithm:
-    #         # D_X, D_Y, A_Y, A_X = main(X, Y, w)
-    #         print("testing with {} and {}".format(ft, tr))
-    #         D_X, D_Y, A_Y, A_X = main(Xsim, Ysim, 200, ft, tr)
+    for tr in tqdm(transform_algorithm):
+        for ft in tqdm(fit_algorithm):
+            for la in tqdm(lambs):
+                # D_X, D_Y, A_Y, A_X = main(X, Y, w)
+                print("testing with {} and {}".format(ft, tr))
+                D_X, D_Y, A_Y, A_X = main_optimize(Xsim, Ysim, w0, ft, tr, lamb=la)
 
 
+    D_X, D_Y, A = joint_optimize(Xsim, Ysim, w0, w1, 'lars', 'lasso_lars')
